f9f94d1fb539dee5b8d48465036835fa9167be43
Kenrick Turner <kenrick@kenners.org>
2015-11-05 22:20:41 +0000

    Version 0.2

--- run_server.py
+++ run_server.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
-# -*- coding: utf-8 -*-
 from sundowner import app
-#app.run(debug=True)
+# app.run(debug=True)
 # Replace above with following line for production
 app.run(host='0.0.0.0')
--- setup.py
+++ setup.py
@@ -1,23 +1,22 @@
 #!/usr/bin/env python3
-# -*- coding: utf-8 -*-
 
 from setuptools import setup
 
 setup(
     name='Sundowner',
-    version='0.1.4',
+    version='0.2',
     long_description=__doc__,
     packages=['sundowner'],
     include_package_data=True,
     zip_safe=False,
     install_requires=[
-        'Flask>=0.10',
-        'ephem>=3.7',
+        'Flask>=0.10.1',
+        'ephem>=3.7.6',
         'itsdangerous>=0.24',
-        'Jinja2>=2.7.2',
+        'Jinja2>=2.8',
         'MarkupSafe>=0.23',
-        'Werkzeug>=0.9.4',
-        'python-dateutil>=2.2'
+        'Werkzeug>=0.10.4',
+        'python-dateutil>=2.4.2'
     ],
     test_suite='sundowner.test'
 )
